07f870ed8ba82bf633b0f8463197eb51a4de3163
Spaetzialist <34004227+Spaetzialist@users.noreply.github.com>
2018-02-09 07:49:27 +0100

    plot added

--- backtestTemplate.js
+++ backtestTemplate.js
@@ -1,24 +1,23 @@
 # mit Ausgabe von Gewinn/Verlust in % 
 # todo:
-# - Integration Parameter für Backtest ändern
-# - maximum buy amount in % vom Gesamtwert
 
 
 trading = require "trading"  
 params = require "params"  
  
 
-_maximumExchangeFee = params.add "Maximum exchange fee %", .25  
-_maximumOrderAmount = params.add "Maximum order amount", 1         #wieviel Stück maximal
-_maximumMoneyPerTrade = params.add "Maximum money per trade", 100   #wieviel Geld pro Trade maximal, 0: so viel wie es geht
-_rebuy = params.add "Buy more than once before selling?", 0         #muss man erst verkaufen um wieder neu zu kaufen oder kann man auch mehrmals hintereinander kaufen?
-                                                                   #1: mehrmals, 0: nur 1x kaufen
-#_orderTimeout = params.add "Order timeout", 30  
+_maximumExchangeFee = .25  
 _orderTimeout = 30   
 MINIMUM_AMOUNT = 0.04
-PERCENT = 0.05
+
 init: ->  
     #This runs once when the bot is started  
+	context.PERCENT = 0.05
+	setPlotOptions
+        performance:
+            color: 'blue'
+            secondary: true
+            size: 5
 handle: ->  
     #This runs once every tick or bar on the graph  
     storage.botStartedAt ?= data.at  
@@ -36,22 +35,23 @@ handle: ->
         maximumBuyAmount = (currencyAvailable/instrument.price) * (1 - (_maximumExchangeFee/100))  
     maximumSellAmount = assetsAvailable  #verkaufe alles was da ist
     
-    
+        
+    plot
+        performance: 100*(currencyAvailable + assetsAvailable * instrument.price)/storage.startBalance
     #---------------------------------------------------------------------------
     #-----------------------------------Strategie-------------------------------
     #---------------------------------------------------------------------------
 
-    if ((assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc*(1+PERCENT)))  
+    if ((assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc*(1+context.PERCENT)))  
         trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
         storage.startKursCalc = instrument.price
     if ((assetsAvailable>0)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc))
         storage.startKursCalc = instrument.price    
-    if ((assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-PERCENT)))  
+    if ((assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-context.PERCENT)))  
         trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
     if ((assetsAvailable == 0)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc))
         storage.startKursCalc = instrument.price 
-        
-    #info  "storage.startKursCalc = #{storage.startKursCalc}"
+    
 onRestart: ->  
     debug "Bot restarted at #{new Date(data.at)}"  
 
@@ -71,8 +71,3 @@ onStop: ->
     info "Bot Profit = #{botProfit.toFixed(2)}%" 
     info "Hodl Profit = #{buhProfit.toFixed(2)}%"
     debug ""
-
-            
-            
-            
-          
