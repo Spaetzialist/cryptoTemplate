003b58cde6cc47ffef8481db984fb2d1f5c557fa
Spaetzialist <34004227+Spaetzialist@users.noreply.github.com>
2018-02-11 06:40:03 +0100

    unnötige Sachen rausgeworfen

--- backtestTemplate.js
+++ backtestTemplate.js
@@ -1,4 +1,4 @@
-# mit Ausgabe von Gewinn/Verlust in % 
+# Trailing Stop
 # todo:
 
 
@@ -6,18 +6,16 @@ trading = require "trading"
 params = require "params"  
  
 
-_maximumExchangeFee = .25  
+_maximumExchangeFee = .26
 _orderTimeout = 30   
-MINIMUM_AMOUNT = 0.04
-
+MINIMUM_AMOUNT = 30
+#######Params
+_einsatz = 1  #maximaler Einsatz prozentual vom Gesamtvermögen
+_trailingStop = params.add "Trailing Stop in %", 10  #%  bei so einem Rückgang steige ich aus
+#######
 init: ->  
     #This runs once when the bot is started  
-	context.PERCENT = 0.05
-	setPlotOptions
-        performance:
-            color: 'blue'
-            secondary: true
-            size: 5
+    context.PERCENT = _trailingStop/100
 handle: ->  
     #This runs once every tick or bar on the graph  
     storage.botStartedAt ?= data.at  
@@ -25,32 +23,26 @@ handle: ->
 
     assetsAvailable = @portfolios[instrument.market].positions[instrument.asset()].amount  
     currencyAvailable = @portfolios[instrument.market].positions[instrument.curr()].amount  
-    storage.startBalance ?= currencyAvailable     #speicher Startkapital für Auswertung am Ende
-    storage.startPrice ?= instrument.price        #speicher initial price für Auswertung am Ende
+
     storage.startKursCalc ?= instrument.price
-    _maximumMoneyPerTrade = currencyAvailable * 0.1 
+    storage.sellKurs ?= instrument.price
+    _maximumMoneyPerTrade = currencyAvailable * _einsatz
     if (_maximumMoneyPerTrade>0)
-        maximumBuyAmount = (_maximumMoneyPerTrade/instrument.price) * (1 - (_maximumExchangeFee/100))  
-    else
-        maximumBuyAmount = (currencyAvailable/instrument.price) * (1 - (_maximumExchangeFee/100))  
+        maximumBuyAmount = (_maximumMoneyPerTrade/instrument.price) * (1 - (_maximumExchangeFee*2/100))  
+    
     maximumSellAmount = assetsAvailable  #verkaufe alles was da ist
     
         
-    plot
-        performance: 100*(currencyAvailable + assetsAvailable * instrument.price)/storage.startBalance
     #---------------------------------------------------------------------------
     #-----------------------------------Strategie-------------------------------
     #---------------------------------------------------------------------------
 
-    if ((assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc*(1+context.PERCENT)))  
-        trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
-        storage.startKursCalc = instrument.price
+    
     if ((assetsAvailable>0)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc))
         storage.startKursCalc = instrument.price    
     if ((assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-context.PERCENT)))  
         trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
-    if ((assetsAvailable == 0)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc))
-        storage.startKursCalc = instrument.price 
+
     
 onRestart: ->  
     debug "Bot restarted at #{new Date(data.at)}"  
@@ -61,13 +53,9 @@ onStop: ->
     debug "Bot stopped at #{new Date(data.at)}"  
 
     assetsAvailable = @portfolios[instrument.market].positions[instrument.asset()].amount   
-    if (assetsAvailable > 0)
-        trading.sell instrument    #verkaufe alles um einen Endpreis zu erhalten
     currentBalance = @portfolios[instrument.market].positions[instrument.base()].amount 
-    botProfit = ((currentBalance / storage.startBalance)*100) 
-    buhProfit = ((instrument.price / storage.startPrice)*100) 
+
     debug "currency = #{currentBalance.toFixed(2)}"
     debug "assets = #{@portfolios[instrument.market].positions[instrument.asset()].amount}"
-    info "Bot Profit = #{botProfit.toFixed(2)}%" 
-    info "Hodl Profit = #{buhProfit.toFixed(2)}%"
+
     debug ""
