ec58fb5e4914912a2ea367f00527cbb98cd2cffd
Spaetzialist <34004227+Spaetzialist@users.noreply.github.com>
2018-02-19 08:44:57 +0100

    + einstoppen

--- backtestTemplate.js
+++ backtestTemplate.js
@@ -12,6 +12,9 @@ MINIMUM_AMOUNT = 30
 #######Params
 _einsatz = 1  #maximaler Einsatz prozentual vom Gesamtvermögen
 _trailingStop = params.add "Trailing Stop in %", 10  #%  bei so einem Rückgang steige ich aus
+_trailingIn = params.add "Trailing In in %", 0
+_kursStop = params.add "Kurs Stop", 0.86
+_kursIn = params.add "Kurs In", 0
 #######
 init: ->  
     #This runs once when the bot is started  
@@ -36,15 +39,28 @@ handle: ->
     #-----------------------------------Strategie-------------------------------
     #---------------------------------------------------------------------------
 
-    #if ((assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc*(1+context.PERCENT)))  
-    #    trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
-    #    storage.startKursCalc = instrument.price
     if (assetsAvailable>0)
         info "Ausstieg bei: #{storage.startKursCalc*(1-context.PERCENT)}"
+    #Einstieg bei einem festen Kurs
+    if ((_kursIn>0)&&(assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>_kursIn))  
+        trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
+        storage.startKursCalc = instrument.price
+    #Einstieg bei einem festen Prozentsatz über Minimum, d.h. wenn der Kurs wieder steigt
+    if ((_trailingIn>0)&&(assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc*(1+context.PERCENT)))  
+        trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
+        storage.startKursCalc = instrument.price
+    #passe den Referenzkurs an den steigenden Kurs an für Trailing Stop
     if ((assetsAvailable>0)&&(instrument.close[instrument.close.length-1]>storage.startKursCalc))
-        storage.startKursCalc = instrument.price    
-    if ((assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-context.PERCENT)))  
+        storage.startKursCalc = instrument.price
+    #Ausstieg bei festem Prozentsatz unter max. Kurs Wert
+    if ((_trailingStop > 0)&&(assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-context.PERCENT)))  
         trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
+        storage.startKursCalc = instrument.price 
+    #Ausstieg bei festem Kurs   
+    if ((_kursStop > 0)&&(assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<_kursStop))  
+        trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
+        storage.startKursCalc = instrument.price 
+    #passe den Referenzkurs an den fallenden Kurs an für Wiedereinstieg
     if ((assetsAvailable == 0)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc))
         storage.startKursCalc = instrument.price 
 
