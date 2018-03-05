260b2bebf533bf258642a8de6802c9f2f27ac85b
Spaetzialist <34004227+Spaetzialist@users.noreply.github.com>
2018-03-05 22:18:59 +0100

    kleinere Anpassungen

--- backtestTemplate.js
+++ backtestTemplate.js
@@ -19,11 +19,12 @@ _trailingStop = params.add "Trailing Stop in %", 5  #%  bei so einem Rückgang s
 _trailingIn = params.add "Trailing In in %", 0
 _kursStop = params.add "Kurs Stop", 0
 _kursIn = params.add "Kurs In", 0
+outputCounter = 15
 #######
 init: ->  
     #This runs once when the bot is started  
     context.PERCENT = _trailingStop/100
-    context.COUNTER = 15
+    context.COUNTER = outputCounter
 handle: ->  
     #This runs once every tick or bar on the graph  
     storage.botStartedAt ?= data.at  
@@ -45,9 +46,11 @@ handle: ->
     #---------------------------------------------------------------------------
 
     if ((assetsAvailable>0)&&(_trailingStop>0))
+        debug context.COUNTER
         if (context.COUNTER == 0)
             info "Ausstieg bei: #{storage.startKursCalc*(1-context.PERCENT)}"
-            context.COUNTER = context.COUNTER - 1
+            context.COUNTER = outputCounter
+        context.COUNTER = context.COUNTER - 1
     #Einstieg bei einem festen Kurs
     if ((_kursIn>0)&&(assetsAvailable==0)&&(maximumBuyAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]>_kursIn))  
         trading.buy instrument, 'market', maximumBuyAmount, instrument.price, _orderTimeout 
@@ -63,7 +66,7 @@ handle: ->
     if ((_trailingStop > 0)&&(assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<storage.startKursCalc*(1-context.PERCENT)))  
         trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
         storage.startKursCalc = instrument.price 
-    #Ausstieg bei festem Kurs   
+    #Ausstieg bei festem Kurs (StopLoss)
     if ((_kursStop > 0)&&(assetsAvailable>0)&&(maximumSellAmount >= MINIMUM_AMOUNT)&&(instrument.close[instrument.close.length-1]<_kursStop))  
         trading.sell instrument, 'market', maximumSellAmount, instrument.price, _orderTimeout 
         storage.startKursCalc = instrument.price 
